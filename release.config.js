e509ea344e0f339ca0e5cc8068a1718c1b611ec8
nayoon030303 <nayoon030303@gmail.com>
2022-08-29 17:17:54 +0900

    fix: release config

--- release.config.js
+++ release.config.js
@@ -16,12 +16,14 @@ module.exports = {
         successComment:
           ':tada: This ${issue.pull_request ? "PR is included" : "issue has been resolved"} in ' +
           +'[version ${nextRelease.version}](${releases.filter(release => /github.com/i.test(release.url))[0].url}) :tada:',
-    }],
+      },
+    ],
     [
       '@semantic-release/exec',
       {
         successCmd:
           'echo "RELEASE_VERSION=${nextRelease.version}" >> $GITHUB_ENV',
-    }],
+      },
+    ],
   ],
 };
