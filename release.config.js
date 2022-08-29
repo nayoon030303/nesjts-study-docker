836029303dae80ebee05d7d8e52b17c934a29f1f
Belle <56942566+nayoon030303@users.noreply.github.com>
2022-08-29 17:19:57 +0900

    fix: release config (#41)
    * fix: release config
    
    * fix: develop

--- release.config.js
+++ release.config.js
@@ -3,7 +3,7 @@
  * https://github.com/semantic-release/semantic-release/blob/master/docs/extending/plugins-list.md
  */
 module.exports = {
-  branches: ['master', 'next'],
+  branches: ['master', 'develop'],
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
@@ -16,12 +16,14 @@ module.exports = {
         successComment:
           ':tada: This ${issue.pull_request ? "PR is included" : "issue has been resolved"} in ' +
           +'[version ${nextRelease.version}](${releases.filter(release => /github.com/i.test(release.url))[0].url}) :tada:',
-    }],
+      },
+    ],
     [
       '@semantic-release/exec',
       {
         successCmd:
           'echo "RELEASE_VERSION=${nextRelease.version}" >> $GITHUB_ENV',
-    }],
+      },
+    ],
   ],
 };
