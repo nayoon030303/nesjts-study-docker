8f93da16db2bb093a1281757fb3d0cb50b9b10f4
nayoon030303 <nayoon030303@gmail.com>
2022-09-20 17:11:56 +0900

    fix: key

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,13 +1,14 @@
 import { test, expect } from '@playwright/test';
 
-test('homepage has Playwright in title and get started link linking to the intro page', async ({ page }) => {
+test('homepage has Playwright in title and get started link linking to the intro page', async ({
+  page,
+}) => {
   await page.goto('https://playwright.dev/');
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Playwright/);
 
   const getStarted = page.locator('text=Get Started');
-
 
   await expect(getStarted).toHaveAttribute('href', '/docs/intro');
 
